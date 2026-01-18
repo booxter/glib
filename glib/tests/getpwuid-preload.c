93deb4832f9d8b541079e5143292db036764a3e9
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-01-18 12:20:10 -0500

    Allocate from heap

--- glib/tests/getpwuid-preload.c
+++ glib/tests/getpwuid-preload.c
@@ -20,12 +20,12 @@
  * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "config.h"
 #include <dlfcn.h>
 #include <glib.h>
 #include <pwd.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "config.h"
 
 #ifndef __APPLE__
 
